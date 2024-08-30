f08b44ce1f8fdb87c36ddb941d9a2cb916b3d956
Lukas-Rodrigo <lucasrodrigo0503@gmail.com>
2024-08-30 13:40:18 -0300

    componente HOME adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,12 +23,14 @@ import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { NavComponent } from './components/nav/nav.component';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
